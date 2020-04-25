791ca54612ebf111809fc030471c39d455c9bc70
Manuel Sidler <manuel@sidler.io>
2020-04-25 20:17:02 +0200

    load and display word document metadata

---
+++ src/Data/DocumentMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorOfficeAddIn.Data
+{
+    public class DocumentMetadata
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("subject")]
+        public string Subject { get; set; }
+    }
+}
---
+++ src/wwwroot/js/officeWrapper.js
@@ -0,0 +1,19 @@
+﻿window.wordWrapper = {
+    getDocumentMetadata: function () {
+        return window.Word.run(function (context) {
+            var properties = context.document.properties;
+            context.load(properties);
+
+            return context.sync()
+                .then(function () {
+                    return {
+                        title: properties.title,
+                        subject: properties.subject
+                    };
+                })
+                .catch(function (error) {
+                    return error;
+                });
+        });
+    }
+}
